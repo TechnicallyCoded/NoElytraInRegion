16fabe41c1406f066eb9e43c5ebbabc8f5f75ac8
TechnicallyCoded <technicallycoded@gmail.com>
2023-04-12 14:47:58 +0200

    Handle toggle

--- src/main/java/com/tcoded/noelytrainregion/EventListener.java
+++ src/main/java/com/tcoded/noelytrainregion/EventListener.java
@@ -5,6 +5,7 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityToggleGlideEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 public class EventListener implements Listener {
@@ -28,11 +29,27 @@ public class EventListener implements Listener {
         }
 
         // Check if player is in region
-        if (plugin.getNoElytraRegions().stream().anyMatch(region -> region.contains(to.getBlockX(), to.getBlockY(), to.getBlockZ()))) {
+        if (isInBannedRegion(to)) {
             Player player = event.getPlayer();
+            if (!player.isGliding()) return;
+
             player.setGliding(false);
             player.sendMessage(ChatColor.RED + "You can't use an elytra in this region!");
         }
+    }
+
+    @EventHandler
+    public void onElytra(EntityToggleGlideEvent event) {
+        if (!(event.getEntity() instanceof Player)) return;
+
+        if (event.isGliding() && isInBannedRegion(event.getEntity().getLocation())) {
+            event.setCancelled(true);
+            event.getEntity().sendMessage(ChatColor.RED + "You can't use an elytra in this region!");
+        }
+    }
+
+    private boolean isInBannedRegion(Location to) {
+        return plugin.getNoElytraRegions().stream().anyMatch(region -> region.contains(to.getBlockX(), to.getBlockY(), to.getBlockZ()));
     }
 
 }
